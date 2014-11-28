d1229792da1775b9e59449d11408b7fcc180d179
clungu1 <clungu1@hawk.iit.edu>
2014-11-28 15:14:56 -0600

    branch test
    this is to test and see if it commits only to my branch

--- src/MainPack/Application.java
+++ src/MainPack/Application.java
@@ -2,4 +2,8 @@ package MainPack;
 
 public class Application {
 
+	public static void main(String args[])
+	{
+		System.out.println("test");
+	}
 }
---
+++ src/MainPack/Product.java
@@ -0,0 +1,45 @@
+package MainPack;
+
+public class Product {
+
+	private int barCode;
+	private double price;
+	
+	public Product()
+	{
+		barCode = 0;
+		price = 0;
+	}
+	
+	public Product(int b, double p)
+	{
+		barCode = b;
+		price = p;
+	}
+
+	public void setBarCode(int b)
+	{
+		barCode = b;
+	}
+	
+	public void setPrice(double p)
+	{
+		price = p;
+	}
+	
+	public int getBarCode()
+	{
+		return barCode;
+	}
+
+	public double getPrice()
+	{
+		return price;
+	}
+	
+	public String toString()
+	{
+		return "";
+	}
+}
+
