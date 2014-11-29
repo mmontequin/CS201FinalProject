a1fe7cafcf832d25f7bad716fd09933878799686
clungu1 <clungu1@hawk.iit.edu>
2014-11-28 19:12:38 -0600

    commit2

--- src/MainPack/Calculations.java
+++ src/MainPack/Calculations.java
@@ -8,10 +8,11 @@
 package MainPack;
 
 public class Calculations extends Object{
-	
+
+/*
 	private Object arr[] = super.getArray();
-/*	
-	public void sortByBarcode()
+
+	public void sort(int c)
 	{
 		Object aux;
 		int min = 999, poz;
@@ -19,9 +20,31 @@ public class Calculations extends Object{
 		for(int i = 0; i < super.getIndex() - 1; i ++)
 		{
 			for(int j = i; j < super.getIndex(); j ++)
-				if(arr[j].getBarCode() < min)
+			{	
+				if( arr[j].getBarCode() < min)
 				{
-					min = arr[j].barCode;
+					min = arr[j].getBarCode();
+					poz = j;
+				}
+			}
+			aux = arr[i];
+			arr[i] = arr[poz];
+			arr[poz] = aux;
+		}
+	}
+
+	public void sort(double d)
+	{
+		Object aux;
+		int poz;
+		double min;
+		
+		for(int i = 0; i < super.getIndex() - 1; i ++)
+		{
+			for(int j = i; j < super.getIndex(); j ++)
+				if(arr[j].getPrice() < min)
+				{
+					min = arr[j].getPrice();
 					poz = j;
 				}
 			aux = arr[i];
@@ -29,5 +52,13 @@ public class Calculations extends Object{
 			arr[poz] = aux;
 		}
 	}
+	
+	public Object search(int b)
+	{
+		for(int i = 0; i < super.getIndex(); i ++)
+			if(b == arr[i].getBarCode())
+				return arr[i];
+	}
 */
+	
 }
