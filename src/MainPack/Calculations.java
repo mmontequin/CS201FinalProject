5dee98b7d8f922476cc60145f19f6b0eecba0fe7
clungu1 <clungu1@hawk.iit.edu>
2014-11-28 18:08:47 -0600

    commit

--- src/MainPack/Calculations.java
+++ src/MainPack/Calculations.java
@@ -1,5 +1,33 @@
+/* 
+ * Cosmin Lungu
+ * CS 201 - 01
+ * Final Project
+ * working with Matt Montequin
+ */
+
 package MainPack;
 
-public class Calculations {
-
+public class Calculations extends Object{
+	
+	private Object arr[] = super.getArray();
+/*	
+	public void sortByBarcode()
+	{
+		Object aux;
+		int min = 999, poz;
+		
+		for(int i = 0; i < super.getIndex() - 1; i ++)
+		{
+			for(int j = i; j < super.getIndex(); j ++)
+				if(arr[j].getBarCode() < min)
+				{
+					min = arr[j].barCode;
+					poz = j;
+				}
+			aux = arr[i];
+			arr[i] = arr[poz];
+			arr[poz] = aux;
+		}
+	}
+*/
 }
